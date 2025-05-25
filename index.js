0d5a4a3e56408963294fa576edd5a47931c2396a
Md Jamshed Uddin <jamsheduddin03@gmail.com>
2025-05-25 11:06:13 +0600

    file structure setup

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const connectdb = require("./config/db");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// connectdb()
 
 app.get("/", (req, res) => {
   res.status(200).send({ message: "Server is running" });
