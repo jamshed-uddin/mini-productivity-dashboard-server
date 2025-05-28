49d3a8db8b827d154463ddcb600395eadb8df6e2
Md Jamshed Uddin <jamsheduddin03@gmail.com>
2025-05-29 03:23:15 +0600

    cookie samesite to Strict

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = ({ res, userId }) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "None",
+    sameSite: "Strict",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
