832f028169559471d24a37cc0f549c1b9a3e3e9b
Md Jamshed Uddin <jamsheduddin03@gmail.com>
2025-05-29 14:19:50 +0600

    wrapped entry file with async function

--- index.js
+++ index.js
@@ -11,30 +11,39 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-connectdb();
+const StartServer = async () => {
+  try {
+    await connectdb();
 
-app.use(cookieParser());
-app.use(express.json());
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://stride-eta.vercel.app"],
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    credentials: true,
-  })
-);
+    app.use(cookieParser());
+    app.use(express.json());
+    app.use(
+      cors({
+        origin: ["http://localhost:3000", "https://stride-eta.vercel.app"],
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+        credentials: true,
+      })
+    );
 
-app.get("/", (req, res) => {
-  res.status(200).send({ message: "Server is running" });
-});
+    app.get("/", (req, res) => {
+      res.status(200).send({ message: "Server is running" });
+    });
 
-// api endpoints
-app.use("/api/users", userRoutes);
-app.use("/api/tasks", taskRoutes);
-app.use("/api/goals", goalRoutes);
+    // api endpoints
+    app.use("/api/users", userRoutes);
+    app.use("/api/tasks", taskRoutes);
+    app.use("/api/goals", goalRoutes);
 
-app.use(notFound);
-app.use(errorHandler);
+    app.use(notFound);
+    app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port - ${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Server running on port - ${PORT}`);
+    });
+  } catch (error) {
+    console.log("server error -", error);
+    process.exit(1);
+  }
+};
+
+StartServer();
